65ca66124f8856f102419f33069e1b1889c9f56c
blazeak08 <105526703+blazeak08@users.noreply.github.com>
2022-05-13 20:44:21 -0400

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,13 @@
+# Ask for name
+# Say Hello, name. What is your age
+# Use if else
+
+name = input("What is your name?") 
+age = input("Hello, " +  name + " how old are you?")
+print(name)
+print(age)
+
+if float(age) > 10:
+     print("Welcome back.") 
+else:
+    print("You should not be on the computer, go outside to play. ")
